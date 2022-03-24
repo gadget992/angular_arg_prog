429da1dcca8c0258ec5f34519124ef67b3268ec1
gadget992 <fernandezdavid1992@gmail.com>
2022-03-23 21:11:09 -0300

    agregado mock e interface

---
+++ task-list/src/app/mock-tasks.ts
@@ -0,0 +1,26 @@
+export const TASKS = [
+    {
+        id : 1,
+        text : 'Terminar primer modulo de angular',
+        day : 'Agosto 5 a las 12:00 hs',
+        reminder : true,
+    },
+    {
+        id : 2,
+        text : 'Arreglar la casa',
+        day : 'Agosto 2 a las 19:00 hs',
+        reminder : false,
+    },
+    {
+        id : 3,
+        text : 'Investigar sobre bootstraps',
+        day : 'Septiembre 2 a las 05:00 hs',
+        reminder : true,
+    },
+    {
+        id : 4,
+        text : 'Dar de comer a los animales',
+        day : 'Enero 25 a las 12:00 hs',
+        reminder : false,
+    }
+]
